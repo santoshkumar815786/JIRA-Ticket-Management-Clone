9d3d56fbe129766a1309c07cc553cf6f62a32897
Santosh Kumar <santoshkumar815786@gmail.com>
2021-10-19 08:22:35 +0530

    Generating sample ticket when shift key pressed inside modal
    	- When user press shift key inside create-ticket-modal a new
    	  ticket will be created and appended as child inside main-cont
    
    	- Right now ticket has static value i.e a ticket with dummy data
    	  will be generated again and agian.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let btnCreateTicket = document.querySelector(".ticket-action-create");
 
 let ticketModalCont = document.querySelector(".ticket-modal-cont");
 
+let displayTicketsMainCont = document.querySelector(".main-cont");
+
+let textareaTicketDescription = document.querySelector(".textarea-ticket-description");
+
 let isTicketModalOpen = false;
 
 btnCreateTicket.addEventListener("click", (e)=>{
@@ -17,3 +21,24 @@ btnCreateTicket.addEventListener("click", (e)=>{
         btnCreateTicket.classList.remove("border");
     }
 });
+
+
+textareaTicketDescription.addEventListener("keydown",(e)=>{
+    if(e.key === "Shift")
+    {
+        createNewTicket("color-chartreuse","#SAN5526K","Sample ticket generation");
+    }
+});
+
+
+function createNewTicket(ticketColor, ticketId, ticketTaskDescription)
+{
+    let newTicket = document.createElement("div");
+    newTicket.setAttribute("class", "display-ticket-cont"); 
+    newTicket.innerHTML = `<div class="${ticketColor} display-ticket-color-cont"></div>
+    <div class="display-ticket-id-cont">${ticketId}</div>
+    <div class="display-ticket-description-cont">
+        ${ticketTaskDescription}
+    </div>`;
+    displayTicketsMainCont.appendChild(newTicket);
+}
