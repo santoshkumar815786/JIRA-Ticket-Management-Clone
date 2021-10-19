248fbbed4cf78d14086fe4ee2990ed5a5bcd2722
Santosh Kumar <santoshkumar815786@gmail.com>
2021-10-19 08:48:20 +0530

    New function added ticketModalShowAndHideController()
    	- This function will simply controll the hiding and showing of
    	  .ticket-modal-cont based on the value of shouldShow(boolean)
    
    	- Also it will clear the content of ticket-task-description
    	  before hiding .ticket-modal-cont
    
    	- It will remove the class property .border from the class list of
    	  .ticket-action-create

--- script.js
+++ script.js
@@ -8,37 +8,43 @@ let textareaTicketDescription = document.querySelector(".textarea-ticket-descrip
 
 let isTicketModalOpen = false;
 
-btnCreateTicket.addEventListener("click", (e)=>{
+btnCreateTicket.addEventListener("click", (e) => {
     isTicketModalOpen = !isTicketModalOpen;
-    if(isTicketModalOpen)
-    {
-        ticketModalCont.style.display = "flex";
-        btnCreateTicket.classList.add("border");
-    }
-    else
-    {
-        ticketModalCont.style.display = "none";
-        btnCreateTicket.classList.remove("border");
+    ticketModalShowAndHideController(isTicketModalOpen);
+});
+
+
+textareaTicketDescription.addEventListener("keydown", (e) => {
+    if (e.key === "Shift") {
+        createNewTicket("color-chartreuse", "#SAN5526K", "Sample ticket generation");
     }
 });
 
 
-textareaTicketDescription.addEventListener("keydown",(e)=>{
-    if(e.key === "Shift")
-    {
-        createNewTicket("color-chartreuse","#SAN5526K","Sample ticket generation");
-    }
-});
-
-
-function createNewTicket(ticketColor, ticketId, ticketTaskDescription)
-{
+function createNewTicket(ticketColor, ticketId, ticketTaskDescription) {
     let newTicket = document.createElement("div");
-    newTicket.setAttribute("class", "display-ticket-cont"); 
+    newTicket.setAttribute("class", "display-ticket-cont");
     newTicket.innerHTML = `<div class="${ticketColor} display-ticket-color-cont"></div>
     <div class="display-ticket-id-cont">${ticketId}</div>
     <div class="display-ticket-description-cont">
         ${ticketTaskDescription}
     </div>`;
     displayTicketsMainCont.appendChild(newTicket);
+
+    isTicketModalOpen = false;  // Setting false as closing the create-ticket-modal
+    ticketModalShowAndHideController(isTicketModalOpen);
+}
+
+function ticketModalShowAndHideController(shouldShow) 
+{
+    // This function will simply controll the hiding and showing of .ticket-modal-cont based on the value of shouldShow
+    if (shouldShow) {
+        ticketModalCont.style.display = "flex";
+        btnCreateTicket.classList.add("border");
+    }
+    else {
+        textareaTicketDescription.value = "";   // Setting ticket task description textarea as "" i.e empty
+        ticketModalCont.style.display = "none"; // Hiding create-ticket-modal
+        btnCreateTicket.classList.remove("border"); // Removing border from the class list of .ticket-action-create
+    }
 }
